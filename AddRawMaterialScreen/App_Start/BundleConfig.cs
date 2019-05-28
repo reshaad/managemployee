3fa9776944b7077c17068f1d0e4f2c1ba5eb45db
Reshaad Cassim <Reshaad@Reshaads-MacBook-Air.local>
2019-05-28 20:15:40 +0200

    updated sidemenu and table

--- AddRawMaterialScreen/App_Start/BundleConfig.cs
+++ AddRawMaterialScreen/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace AddRawMaterialScreen
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js" , "~/Content/jquery.meanmenu.js", "~/Content/jquery.scrollUp.js", "~/Content/jquery.mCustomScrollbar.concat.min.js" ));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -17,11 +17,13 @@ namespace AddRawMaterialScreen
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js", "~/Content/main.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css"  ));
+
+
         }
     }
 }
